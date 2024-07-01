fcaebd09633e977ef9c7e23e0512f9c97ada56fa
Saeed Aghaee <aghaeester@gmail.com>
2024-07-01 19:50:55 +0100

    Update

--- backend/src/main/java/com/mrwersa/chat_craft/config/WebConfig.java
+++ backend/src/main/java/com/mrwersa/chat_craft/config/WebConfig.java
@@ -1,12 +1,23 @@
+package com.mrwersa.chat_craft.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebFluxConfigurer {
 
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Override
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
